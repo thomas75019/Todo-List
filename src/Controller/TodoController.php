ed9d567a89e6476909c0a7a050772b4f4150dce6
Thomas75019 <tlarousse3@gmail.com>
2018-12-25 21:39:07 +0100

    Home method and Add method

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -1,1 +1,24 @@
+<?php
+namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TodoController extends AbstractController
+{
+    /**
+     * @Route("/", name="todo_homel")
+     */
+    public function home()
+    {
+        return $this->render('index.html.twig');
+    }
+
+    /**
+     * @Route("/add", name="todo_add")
+     */
+    public function add()
+    {
+
+    }
+}
