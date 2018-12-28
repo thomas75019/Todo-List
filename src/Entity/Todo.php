aff7501abfff74aa11961f77cad297aeb64bc7f8
Thomas75019 <tlarousse3@gmail.com>
2018-12-28 22:54:51 +0100

    add Asserts

--- src/Entity/Todo.php
+++ src/Entity/Todo.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TodoRepository")
@@ -18,11 +20,15 @@ class Todo
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Length(max=20)
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank
+     * @Assert\Length(max=100)
      */
     private $content;
 
